39f80f2a6e93cbd324c729bdef4a4ae92947e19d
NguyenVietThang9 <nguyenvietthangptit@gmail.com>
2024-02-06 14:23:16 +0700

    Add a.cpp

---
+++ a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+template <class temp>
+temp thuong(temp x1, temp x2){
+    return (x1*1.0)/x2;
+
+}
+
+int main(){
+
+   int x1=1, x2=2;
+   cout<<thuong(x1,x2);
+   return 0;
+}
